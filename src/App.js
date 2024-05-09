d47be55db02ca3be489890a9329f71ffe91aca70
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-05-09 16:44:21 +0900

    Add CountComponent

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-// import BlinkCompnent from './components/BlinkCompnent';
+import BlinkCompnent from './components/BlinkCompnent';
 import CountComponent from './components/CountComponent';
 // import CaptionImage from './components/CaptionImage';
 // import HelloWorld from './components/HelloWorld';
+import { useState } from 'react';
 
 function App() {
+  const [showCount, setShowCount] = useState(true);
+
   return (
     <div className="App">
+      <button onClick={e=>{
+        setShowCount(!showCount);
+      }}>버튼</button>
+      {showCount ? <CountComponent/> : null}
       {/* <HelloWorld/> */}
       {/* <CaptionImage imgUrl="https://i.namu.wiki/i/onDXLSQD5sdRk-Z4kJH_5FmLs41RuZhqW9qouHiqR_Sk15NzgC3kCIcwsrH4tAx8d7nCy-ywqxT8BXje6DmHEw.webp" caption="이건 트럭입니다."/> */}
-      {/* <BlinkCompnent text="hi용"/> */}
-      <CountComponent/>
+      <BlinkCompnent text="hi용"/>
     </div>
   );
 }
--- src/components/CountComponent.js
+++ src/components/CountComponent.js
@@ -1,16 +1,45 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
+
+// export default function CountComponent() {
+//     const [count, setCount] = useState(0)
+//     const addCount = () => {
+//         setCount(count+1);
+//     }
+
+//     return (
+//         <div>
+//             <div>{count}</div>
+//             <button onClick={addCount}>1 증가</button>
+//         </div>
+//     )
+// }
+
+import React, { useEffect, useState } from 'react';
 
 export default function CountComponent() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+
     const addCount = () => {
-        setCount(count+1);
-    }
+        setCount(count + 1);
+    };
+
+    useEffect(() => {
+            console.log("데이터 받아오기! (이 함수는 한번만 실행됩니다.)");
+        return () => {
+            console.log("메모리를 잡아먹으면 리소스 해제 하는 함수를 return 해 주어야 합니다.");
+        };
+    }, []);
+    // useeffect의 return값은 랜더링할때와 useeffect가 종료될때 실행되는 함수
+
+    useEffect(() => {
+        console.log(`카운트가 증가할때마다 실행 ! \n -count: ${count}`);
+    }, [count]);
 
     return (
         <div>
-            <div>{count}</div>
-            <button onClick={addCount}>1 증가</button>
+        <div>{count}</div>
+        <button onClick={addCount}>1 증가</button>
         </div>
-    )
-}
+    );
+    }
