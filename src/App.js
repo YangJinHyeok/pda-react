33bb23eb29e76eadde9352cef2eb0baf3eecd96e
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-05-09 15:32:56 +0900

    Add CountComponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import CaptionImage from './components/CaptionImage';
-import HelloWorld from './components/HelloWorld';
+// import BlinkCompnent from './components/BlinkCompnent';
+import CountComponent from './components/CountComponent';
+// import CaptionImage from './components/CaptionImage';
+// import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       {/* <HelloWorld/> */}
-      <CaptionImage imgUrl="https://i.namu.wiki/i/onDXLSQD5sdRk-Z4kJH_5FmLs41RuZhqW9qouHiqR_Sk15NzgC3kCIcwsrH4tAx8d7nCy-ywqxT8BXje6DmHEw.webp" caption="이건 트럭입니다."/>
+      {/* <CaptionImage imgUrl="https://i.namu.wiki/i/onDXLSQD5sdRk-Z4kJH_5FmLs41RuZhqW9qouHiqR_Sk15NzgC3kCIcwsrH4tAx8d7nCy-ywqxT8BXje6DmHEw.webp" caption="이건 트럭입니다."/> */}
+      {/* <BlinkCompnent text="hi용"/> */}
+      <CountComponent/>
     </div>
   );
 }
---
+++ src/components/BlinkCompnent.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+
+
+export default function BlinkCompnent({text}) {
+    const [showText, setShowText] = useState(true);
+    //const X = useState(초기값)
+    // X[0] : State 그 자체, X[1] : State를 변경할 수 있는 함수
+    
+    useEffect(()=>{
+        const timeoutId = setInterval(()=>{
+            setShowText(showText=>!showText);
+        }, 1000)
+        
+        return () => {clearInterval(timeoutId)}
+    }, [])
+
+    return (
+        <div>
+            {showText? text : null}
+        </div>
+    )
+}
+
+
+// import React, { Component, createRef } from 'react'
+
+// export default class CBlinkCompoonent extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             showText: true
+//         }
+//         this.intervalRef = createRef();
+//     }
+//     componentDidMount(){
+//         this.intervalRef.current = setInterval(()=>{
+//         this.setState({showText: !this.state.showText});
+//     }, 1000)
+//     }
+//     componentWillUnmount(){
+//         clearInterval(this.intervalRef.current)
+//     }
+//     render() {
+//     return (
+//         <div> {this.state.showText? this.props.text : null}</div>
+//         )
+//     }
+// }
