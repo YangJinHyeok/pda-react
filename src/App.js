f51f0b2d03445e1288eec530691ddac7379c144a
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-05-09 14:14:30 +0900

    Update CaptionImage.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import HelloWorld from './components/HelloWorld';
 function App() {
   return (
     <div className="App">
-      <HelloWorld/>
-      <CaptionImage/>
+      {/* <HelloWorld/> */}
+      <CaptionImage imgUrl="https://i.namu.wiki/i/onDXLSQD5sdRk-Z4kJH_5FmLs41RuZhqW9qouHiqR_Sk15NzgC3kCIcwsrH4tAx8d7nCy-ywqxT8BXje6DmHEw.webp" caption="이건 트럭입니다."/>
     </div>
   );
 }
--- src/components/CaptionImage.js
+++ src/components/CaptionImage.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-export default function CaptionImage(props) {
+// export default function CaptionImage(props) {
+//     return (
+//         <div style={{textAlign:"center", fontSize:"50px"}}>
+//             <img src={props.imgUrl} alt={props.caption} />
+//             <p>{props.caption}</p>
+//         </div>
+//         )
+//     }
+
+export default function CaptionImage({imgUrl, caption}) {
     return (
-        <div>
-            <img src={props.imgUrl} alt={props.caption} />
-            <p>{props.caption}</p>
+        <div style={{textAlign:"center", fontSize:"50px"}}>
+            <img src={imgUrl} alt={caption} />
+            <p>{caption}</p>
         </div>
         )
     }
