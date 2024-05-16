72b3cf54577ab41b52a77c0038c00c8451a98bd1
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-05-16 14:47:46 +0900

    Feat Board

--- src/App.js
+++ src/App.js
@@ -10,40 +10,42 @@
 // import { ThemeProvider } from "./components/Practice/5.13/ThemeProvider";
 // import Button from 'react-bootstrap/Button';
 // import TodoApp from "./components/TodoList/TodoApp";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Boardapp from "./components/Board/Boardapp";
+import NavBar from "./components/Board/NavBar";
 // import withUser from '../src/components/Practice/5.14/withUser'
 // import UserDisplay from '../src/components/Practice/5.14/UserDisplay';
 
 // const UserDisplayWithUser = withUser(UserDisplay);
 
 function App() {
-    // const [showCount, setShowCount] = useState(true);
+  // const [showCount, setShowCount] = useState(true);
 
-    return (
-        <div className="App">
-            {/* <button onClick={(e) => {setShowCount(!showCount);}}>
+  return (
+    <div className="App">
+      {/* <button onClick={(e) => {setShowCount(!showCount);}}>
                 버튼
             </button>
             {showCount ? <CountComponent /> : null} */}
-            {/* <HelloWorld/> */}
-            {/* <CaptionImage imgUrl="https://i.namu.wiki/i/onDXLSQD5sdRk-Z4kJH_5FmLs41RuZhqW9qouHiqR_Sk15NzgC3kCIcwsrH4tAx8d7nCy-ywqxT8BXje6DmHEw.webp" caption="이건 트럭입니다."/> */}
-            {/* <BlinkCompnent text="hi용"/> */}
-            {/* <FocusInputButton /> */}
-            {/* <PrimeCalculator text={4} /> */}
-            {/* <ThemeProvider>
+      {/* <HelloWorld/> */}
+      {/* <CaptionImage imgUrl="https://i.namu.wiki/i/onDXLSQD5sdRk-Z4kJH_5FmLs41RuZhqW9qouHiqR_Sk15NzgC3kCIcwsrH4tAx8d7nCy-ywqxT8BXje6DmHEw.webp" caption="이건 트럭입니다."/> */}
+      {/* <BlinkCompnent text="hi용"/> */}
+      {/* <FocusInputButton /> */}
+      {/* <PrimeCalculator text={4} /> */}
+      {/* <ThemeProvider>
               <Button variant="primary" size="lg">기본</Button>
               <Button variant="danger">danger</Button>
               <Button variant="info">info</Button>
               <MyPage/>
               <ThemeButton/>
             </ThemeProvider> */}
-            {/* <h1 style={{ textAlign: "center" }}>Todo App</h1>
+      {/* <h1 style={{ textAlign: "center" }}>Todo App</h1>
             <TodoApp /> */}
-            <Boardapp/>
-            {/* <UserDisplayWithUser userId={1} /> */}
-        </div>
-    );
+      {/* <UserDisplayWithUser userId={1} /> */}
+      <NavBar />
+      <Boardapp />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Board/Boardapp.js
+++ src/components/Board/Boardapp.js
@@ -1,85 +1,137 @@
 import React, { useEffect, useState } from "react";
 import Style from "../css/Board.module.css";
+import axios from "axios";
+import { Modal, Button, Form } from "react-bootstrap";
 
 export default function Boardapp() {
-  const [list, setList] = useState([]);
-  const [editIndex, setEditIndex] = useState(null);
-  const [editTitle, setEditTitle] = useState("");
+  const [list, setList] = useState([]); // 리스트 관리
+  const [editTitle, setEditTitle] = useState(""); // 제목 수정
+  const [editBody, setEditBody] = useState(""); // 내용 수정
+  const [showModal, setShowModal] = useState(false); // 내용 보여주기
+  const [showForm, setShowForm] = useState(false); // 수정하기 폼
+  const [selectedItem, setSelectedItem] = useState(null); // 현재 선택된 리스트
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
-      .then((json) => setList(json));
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => setList(response.data));
   }, []);
 
   const handleDelete = (index) => {
-    setList(list.filter((_, i) => i !== index));
-  };
-
-  const handleEdit = (index) => {
-    setEditIndex(index);
-    setEditTitle(list[index].title);
+    const updatedList = list.filter((_, i) => i !== index);
+    updatedList.forEach((item, i) => {
+      item.id = i + 1;
+    });
+    setList(updatedList);
+    setShowForm(false);
+    setShowModal(false);
+    setSelectedItem(null);
+    setEditTitle("");
+    setEditBody("");
   };
 
   const handleSave = (index) => {
     const updatedList = [...list];
     updatedList[index].title = editTitle;
+    updatedList[index].body = editBody;
     setList(updatedList);
-    setEditIndex(null);
+    setShowForm(false);
+    setShowModal(false);
+    setSelectedItem(null);
     setEditTitle("");
+    setEditBody("");
+    handleShowModal(index);
   };
 
-  const handleCancel = () => {
-    setEditIndex(null);
-    setEditTitle("");
+  const handleShowModal = (index) => {
+    setSelectedItem(list[index]);
+    setEditTitle(list[index].title);
+    setEditBody(list[index].body);
+    setShowModal(true);
+    setShowForm(false); // Reset showForm to false when a new item is clicked
   };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedItem(null);
+    setEditTitle("");
+    setShowForm(false);
+  };
+
+  const activeEnter = (e) => {
+    if (e.key === "Enter") {
+      handleSave(selectedItem.id - 1);
+    }
+  };
+
+  const handleShowForm = () => setShowForm(true);
 
   return (
     <div className={Style.background}>
       <ul>
         {list.map((item, index) => (
-          <li key={index} className={Style.card}>
-            {editIndex === index ? (
-              <div className={Style.edit_input}>
-                <input
-                  type="text"
-                  value={editTitle}
-                  onChange={(e) => setEditTitle(e.target.value)}
-                  className={Style.input}
-                />
-                <img
-                  src={`${process.env.PUBLIC_URL}/img/confirm.png`}
-                  className={Style.saveButton}
-                  alt="confirm"
-                  onClick={() => handleSave(index)}
-                />
-                <img
-                  src={`${process.env.PUBLIC_URL}/img/cancel.png`}
-                  className={Style.cancelButton}
-                  alt="cancel"
-                  onClick={handleCancel}
-                />
-              </div>
-            ) : (
-              <div className={Style.modern_input}>
-                <div className={Style.title}>{item.title}</div>
-                <img
-                  src={`${process.env.PUBLIC_URL}/img/edit.png`}
-                  className={Style.editButton}
-                  alt="edit"
-                  onClick={() => handleEdit(index)}
-                />
-                <img
-                  src={`${process.env.PUBLIC_URL}/img/delete.png`}
-                  className={Style.deleteButton}
-                  alt="delete"
-                  onClick={() => handleDelete(index)}
-                />
-              </div>
-            )}
+          <li key={index} className={Style.card} onClick={() => handleShowModal(index)}>
+            <div className={Style.modern_input}>
+              <div className={Style.title}>{item.id}번</div>
+              <br></br>
+              <div className={Style.title}>제목 : {item.title}</div>
+            </div>
           </li>
         ))}
       </ul>
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>내용</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedItem &&
+            (showForm ? (
+              <Form onKeyDown={activeEnter}>
+                <Form.Group className="mb-3">
+                  <Form.Label>제목</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={editTitle}
+                    onChange={(e) => setEditTitle(e.target.value)}
+                    autoFocus
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>내용</Form.Label>
+                  <Form.Control as="textarea" rows={4} value={editBody} onChange={(e) => setEditBody(e.target.value)} />
+                </Form.Group>
+              </Form>
+            ) : (
+              <div>
+                <p>
+                  제목: <br />
+                  <br />
+                  {selectedItem.title}
+                </p>
+                <br />
+                <br />
+                <p>
+                  내용: <br />
+                  <br />
+                  {selectedItem.body}
+                </p>
+              </div>
+            ))}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => handleDelete(selectedItem.id - 1)}>
+            삭제하기
+          </Button>
+          {showForm ? (
+            <Button variant="primary" onClick={() => handleSave(selectedItem.id - 1)}>
+              수정 완료
+            </Button>
+          ) : (
+            <Button variant="primary" onClick={handleShowForm}>
+              수정하기
+            </Button>
+          )}
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
